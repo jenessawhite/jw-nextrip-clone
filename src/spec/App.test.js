2bbebe79737d9b8dc85c0eb4ef650a6a9fadff27
Jenessa White <jenessaw31@gmail.com>
2020-04-12 14:27:41 -0500

    test for overall app

--- src/spec/App.test.js
+++ src/spec/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from '../App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+test('renders learn react link', async () => {
+  const { findAllByText } = render(
+    <Router>
+      <App />
+    </Router>
+  )
+  // the app by default loads the home page and has a link to the home page
+  // so there should be 2 items in the array
+  const items = await findAllByText(/home/i)
+  expect(items).toHaveLength(2)
 })
