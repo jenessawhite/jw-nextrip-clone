e4bb7b9816bbc6a4888bbe744b4818a09f1d7f64
Jenessa White <jenessaw31@gmail.com>
2020-04-13 14:02:38 -0500

    add new Routes to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 import './App.css'
-import Home from './containers/Home'
-import Routes from './containers/Routes'
+import Home from './components/Home'
+import Routes from './components/Routes'
+import Stops from './components/Stops'
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
 
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/routes' component={Routes} />
+        <Route exact path='/routes/:routeId' component={Routes} />
+        <Route exact path='/routes/:routeId/:direction' component={Stops} />
       </Switch>
     </div>
   )
