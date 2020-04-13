01d7765c28f442b09390476d8be5c9b7ce1885dd
Jenessa White <jenessaw31@gmail.com>
2020-04-13 14:00:01 -0500

    getRoutes on Home route

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { getRoutes } from '../API'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+export default function Home() {
+  const [busRoutes, setBusRoutes] = useState([])
+  const [loaded, setLoaded] = useState(false)
+  const [error, setError] = useState('')
+
+  let history = useHistory()
+
+  useEffect(() => {
+    getRoutes()
+      .then((res) => {
+        setBusRoutes(res.data)
+        setLoaded(true)
+      })
+      .catch((err) => {
+        setLoaded(true)
+        setError(err)
+        console.log(err)
+      })
+  }, [])
+
+  const handleChange = (e) => {
+    history.push('/routes/' + e.target.value)
+  }
+
+  if (!loaded && !error) {
+    return <p data-testid='loading'>Loading bus routes...</p>
+  } else if (loaded && error) {
+    return (
+      <div data-testid='error'>
+        <h1>Home</h1>
+        <p>
+          There's something wrong!
+          {console.log(error)}
+        </p>
+      </div>
+    )
+  } else if (loaded && !error) {
+    return (
+      <div data-testid='resolved'>
+        <h1>Home</h1>
+        <p>Please select your route</p>
+        <div>
+          <label htmlFor='routes'>
+            <select
+              name='routes'
+              id='routes'
+              defaultValue='default'
+              onChange={handleChange}>
+              <option value='default' disabled>
+                Select Route
+              </option>
+
+              {busRoutes.map((route) => (
+                <option
+                  key={`transit-route-${route.Route}`}
+                  value={route.Route}>
+                  {route.Description}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default Home
