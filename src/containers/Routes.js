e03f6d8ad6ed98d5e75d47c009899a2c20305e5f
Jenessa White <jenessaw31@gmail.com>
2020-04-13 14:00:40 -0500

    add Routes comp

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { getRoutes } from '../API'
+import { useHistory } from 'react-router-dom'
+import { getRouteDirections } from '../API'
 
-export default function Routes() {
-  const [busRoutes, setBusRoutes] = useState([])
+export default function Routes({ match }) {
+  const [routeDirections, setRouteDirections] = useState([])
   const [loaded, setLoaded] = useState(false)
   const [error, setError] = useState('')
 
+  let history = useHistory()
+
   useEffect(() => {
-    getRoutes()
+    getRouteDirections(match.params.routeId)
       .then((res) => {
-        setBusRoutes(res.data)
+        setRouteDirections(res.data)
         setLoaded(true)
       })
       .catch((err) => {
@@ -17,10 +20,14 @@ export default function Routes() {
         setError(err)
         console.log(err)
       })
-  }, [])
+  }, [match.params.routeId])
+
+  const handleChange = (e) => {
+    history.push('/routes/' + match.params.routeId + '/' + e.target.value)
+  }
 
   if (!loaded && !error) {
-    return <span data-testid='loading'>Loading bus routes...</span>
+    return <span data-testid='loading'>Loading directions...</span>
   } else if (loaded && error) {
     return (
       <span data-testid='error'>
@@ -31,12 +38,32 @@ export default function Routes() {
   } else if (loaded && !error) {
     return (
       <div>
-        <span data-testid='resolved'>There's bus routes!</span>
-        <ul>
-          {busRoutes.map((route) => (
-            <li key={`transit-route-${route.Route}`}>{route.Route}</li>
-          ))}
-        </ul>
+        <span data-testid='resolved'>
+          Your selected route:{` `}
+          {match.params.routeId}
+        </span>
+
+        <div>
+          <label htmlFor='routes'>
+            <select
+              name='direction'
+              id='direction'
+              defaultValue='default'
+              onChange={handleChange}>
+              <option value='default' disabled>
+                Select Direction
+              </option>
+
+              {routeDirections.map((direction) => (
+                <option
+                  key={`transit-direction-${direction.Value}`}
+                  value={direction.Value}>
+                  {direction.Text}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
       </div>
     )
   }
