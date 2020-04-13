eee035a0ee81078acdf151501b17785b97316a68
Jenessa White <jenessaw31@gmail.com>
2020-04-13 14:02:03 -0500

    add api methods for directions and stops

--- src/API.js
+++ src/API.js
@@ -5,3 +5,7 @@ axios.defaults.headers.post['Accept'] = 'application/json'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 export const getRoutes = () => axios.get('/Routes')
+export const getRouteDirections = (routeId) =>
+  axios.get('/Directions/' + routeId)
+export const getStops = (routeId, direction) =>
+  axios.get('/Stops/' + routeId + '/' + direction)
